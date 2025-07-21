14608b4ec8c50e47fc1ab2039c9f42fd88cc1ea6
kBenedek11 <kbenedek611@gmail.com>
2025-07-21 15:29:09 +0200

    player is present on the screen as a triangle part of groups

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    time = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill([0, 0, 0])
+        updatable_group.update(dt)
+        for object in drawable_group:
+            object.draw(screen)
         pygame.display.flip()
+        dt = time.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
